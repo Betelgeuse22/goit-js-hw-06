4bab6fee955fa18a8d47f9ba05dac2cd777937ad
Betelgeuse22 <Don.vitaki@gmail.com>
2022-09-14 23:36:38 +0200

    Update tast 02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientLiEl = document.querySelector("#ingredients");
+
+const ingredientItem = ingredients.map((ingredient) => {
+  const liItem = document.createElement("li");
+  liItem.classList.add("item");
+  liItem.textContent = ingredient;
+
+  return liItem;
+});
+
+ingredientLiEl.append(...ingredientItem);
