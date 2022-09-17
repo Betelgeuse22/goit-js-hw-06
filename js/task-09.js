386c1b80a4cfacd00bd04cf00c16e3b1651ef648
Betelgeuse22 <Don.vitaki@gmail.com>
2022-09-17 23:12:13 +0200

    Update task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnChangeColor = document.querySelector(".change-color");
+
+const bodyEl = document.querySelector("body");
+
+const colorValue = document.querySelector(".color");
+
+btnChangeColor.addEventListener("click", onChangeColor);
+
+function onChangeColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorValue.textContent = bodyEl.style.backgroundColor;
+}
