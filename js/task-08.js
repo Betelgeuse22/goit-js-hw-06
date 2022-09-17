eaf5da0cc6731fda488dcb0504b0a70e0349b9a1
Betelgeuse22 <Don.vitaki@gmail.com>
2022-09-17 18:19:32 +0200

    Update task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+
+  const data = {
+    email: email.value,
+    password: password.value,
+  };
+
+  if (
+    event.currentTarget.elements.email.value.length === 0 ||
+    event.currentTarget.elements.password.value.length === 0
+  ) {
+    alert("Please fill in all fields of the form!");
+  } else {
+    console.log(data);
+  }
+
+  return event.currentTarget.reset();
+}
