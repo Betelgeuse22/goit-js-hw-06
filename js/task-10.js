846b75996aedbe2ace042dc60a4186562ba50ff3
Betelgeuse22 <Don.vitaki@gmail.com>
2022-09-18 00:49:30 +0200

    Update task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,38 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector("input");
+const createBtn = document.querySelector(`button[data-create]`);
+const boxs = document.querySelector("#boxes");
+const destroyBtn = document.querySelector(`button[data-destroy]`);
+
+let amounts = 0;
+const box = [];
+
+input.addEventListener("input", onNumber);
+
+function onNumber() {
+  return (amounts = Number(input.value));
+}
+
+createBtn.addEventListener("click", () => onCreate(amounts));
+
+function onCreate(amounts) {
+  let size = 30;
+
+  for (let i = 0; i < amounts; i += 1) {
+    boxs.insertAdjacentHTML(
+      "beforeend",
+      `<div class="color" style="width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()};"></div>`
+    );
+    size += 10;
+  }
+  boxs.append(...box);
+}
+
+destroyBtn.addEventListener("click", onDestroy);
+
+function onDestroy() {
+  boxs.innerHTML = "";
+}
